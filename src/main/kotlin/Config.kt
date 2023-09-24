8e6230270e6d6bc1529c59064e38a27a8f5c9c69
Mike <mcluck90@gmail.com>
2023-09-24 16:32:45 +0100

    Auto infer tags

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -10,10 +10,13 @@ import java.nio.file.Paths
 data class ProjectConfig(val tags: List<String>)
 
 @Serializable
-data class ConfigFile(val roots: List<String>, val tagIconsDirectory: String, val projects: Map<String, ProjectConfig>)
+data class ConfigFile(val roots: List<String>, val tagIconsDirectory: String, val projects: MutableMap<String, ProjectConfig>)
 
 object Config {
     private lateinit var configFile: ConfigFile
+
+    private val configFilePath: Path
+        get() = Paths.get(System.getenv("HOME"), ".config/codelib.json")
 
     val roots: List<Path>
         get() = configFile.roots.map { Paths.get(it.replace("~", System.getenv("HOME"))) }
@@ -26,24 +29,28 @@ object Config {
     }
 
     fun reload() {
-        val home = System.getenv("HOME")
-        val path = Paths.get(home, ".config/codelib.json")
-
-        if (!Files.exists(path)) {
-            Files.createFile(path)
+        if (!Files.exists(configFilePath)) {
+            Files.createFile(configFilePath)
             val baseConfigFile = ConfigFile(
                 roots = listOf("~/code"),
                 tagIconsDirectory = "~/Pictures/CodeLib Icons",
-                projects = mapOf()
+                projects = mutableMapOf()
             )
-            Files.write(path, Json.encodeToString(baseConfigFile).toByteArray())
+            Files.write(configFilePath, Json.encodeToString(baseConfigFile).toByteArray())
         }
 
-        val json = Files.readString(path)
+        val json = Files.readString(configFilePath)
         configFile = Json.decodeFromString(json)
     }
 
     fun getProject(path: String): ProjectConfig? {
-        return configFile.projects.get(path)
+        return configFile.projects[path]
+    }
+
+    fun setProject(project: Project) {
+        val home = System.getenv("HOME")
+        val path = Paths.get(home, ".config/codelib.json")
+        configFile.projects[project.path.toString()] = ProjectConfig(tags = project.tags)
+        Files.write(path, Json.encodeToString(configFile).toByteArray())
     }
 }
--- src/main/kotlin/Project.kt
+++ src/main/kotlin/Project.kt
@@ -3,11 +3,12 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.exists
+import kotlin.io.path.extension
 import kotlin.io.path.isDirectory
 import kotlin.io.path.isRegularFile
 import kotlin.streams.toList
 
-class Project(private val path: Path) {
+class Project(val path: Path) {
     val name: String
         get() = path.fileName.toString()
 
@@ -26,8 +27,13 @@ class Project(private val path: Path) {
             return Paths.get(tagIconsDir, "Default.png")
         }
 
-    val tags: List<String>
-        get() = Config.getProject(path.toString())?.tags ?: listOf()
+    private var _tags: List<String> = Config.getProject(path.toString())?.tags ?: listOf()
+    var tags: List<String>
+        get() = _tags
+        set(tags) {
+            _tags = tags
+            Config.setProject(this)
+        }
 
     val readme: String
         get() {
@@ -37,6 +43,60 @@ class Project(private val path: Path) {
             }
             return ""
         }
+
+    fun inferTags(): List<String> {
+        fun hasFile(fileName: String): Boolean {
+            return Paths.get(path.toString(), fileName).exists()
+        }
+
+        fun hasFileWithExtension(extension: String): Boolean {
+            return Files.list(path).anyMatch { it.extension == extension }
+        }
+
+        val isC = hasFileWithExtension("c")
+        val isCSharp = hasFileWithExtension("cs") || hasFileWithExtension("csproj")
+        val isDeno = hasFile("deno.jsonc")
+        val isFSharp = hasFileWithExtension("fs") || hasFileWithExtension("fsproj")
+        val isGleam = hasFile("gleam.toml")
+        val isGodot = hasFileWithExtension("tscn")
+        val isHaskell = hasFileWithExtension("hs")
+        val isHtml = hasFileWithExtension("html")
+        val isKotlin = hasFile("build.gradle.kts")
+        val isMint = hasFile("mint.json")
+        val isNodeJs = hasFile("package.json")
+        val isPerl = hasFileWithExtension("pl")
+        val isPython = hasFile("requirements.txt") || hasFileWithExtension("py")
+        val isRust = hasFile("Cargo.toml")
+        val isShell = hasFileWithExtension("sh")
+        val isTypeScript = hasFile("tsconfig.json") || hasFile("tsconfig.base.json")
+        val isUnity = hasFile("ProjectSettings/URPProjectSettings.asset")
+        val isUxn = hasFileWithExtension("tal") || hasFile("uxnasm")
+        val isVisualStudio = hasFileWithExtension("sln")
+        val isWasm = hasFileWithExtension("wat") || hasFileWithExtension("wasm")
+
+        return listOfNotNull(
+            if (isC) "C" else null,
+            if (isCSharp) "C#" else null,
+            if (isDeno) "Deno" else null,
+            if (isFSharp) "F#" else null,
+            if (isGleam) "Gleam" else null,
+            if (isGodot) "Godot" else null,
+            if (isHaskell) "Haskell" else null,
+            if (isHtml) "HTML" else null,
+            if (isKotlin) "Kotlin" else null,
+            if (isMint) "Mint" else null,
+            if (isNodeJs) "Node.js" else null,
+            if (isPerl) "Perl" else null,
+            if (isPython) "Python" else null,
+            if (isRust) "Rust" else null,
+            if (isShell) "Shell" else null,
+            if (isTypeScript) "TypeScript" else null,
+            if (isUnity) "Unity" else null,
+            if (isUxn) "uxn" else null,
+            if (isVisualStudio) "Visual Studio" else null,
+            if (isWasm) "Wasm" else null,
+        )
+    }
 }
 
 fun findProjects(): List<Project> {
@@ -49,7 +109,11 @@ fun findProjects(): List<Project> {
         val directories = allFiles.filter { it.isDirectory() }
         val files = allFiles.filter { it.isRegularFile() }
         if (files.isNotEmpty()) {
-            return listOf(Project(path))
+            var project = Project(path)
+            if (project.tags.isEmpty()) {
+                project.tags = project.inferTags()
+            }
+            return listOf(project)
         }
 
         return directories.flatMap { getProjects(it) }
