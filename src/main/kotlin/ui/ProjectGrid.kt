62f4f7167fce59952360981688e0a16a7d2a2123
Mike <mcluck90@gmail.com>
2023-09-24 16:11:20 +0100

    Improve layout of cards

--- src/main/kotlin/ui/ProjectGrid.kt
+++ src/main/kotlin/ui/ProjectGrid.kt
@@ -3,24 +3,25 @@ package ui
 import Project
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import java.io.File
 
@@ -51,15 +52,29 @@ private fun ProjectCard(project: Project, onClick: (Project) -> Unit) {
         loadImageBitmap(imageFile.inputStream())
     }
 
-    Card {
-        Button(onClick = { onClick(project)}) {
-            Column {
-                Image(
-                    painter = BitmapPainter(image = image),
-                    contentDescription = null
-                )
-                Text(text = project.name)
-            }
+    Card(modifier = Modifier
+        .height(160.dp)
+        .clickable { onClick(project) }
+    ) {
+        Column {
+            Image(
+                painter = BitmapPainter(image = image),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxHeight(0.65f)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = project.name,
+                color = MaterialTheme.colors.onPrimary,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
